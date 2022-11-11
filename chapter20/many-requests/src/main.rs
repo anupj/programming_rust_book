8d7e1f448019a712ae32b43046560ea3ace0f0ef
Anup Jadhav <anup.jadhav@gmail.com>
2022-11-11 11:37:40 +0000

    updated many_requests to use surf crate

--- chapter20/many-requests/src/main.rs
+++ chapter20/many-requests/src/main.rs
@@ -1,27 +1,12 @@
-use async_std::io::prelude::*;
-use async_std::net;
+use async_std::task;
 
-async fn cheapo_request(host: &str, port: u16, path: &str) -> std::io::Result<String> {
-    let mut socket = net::TcpStream::connect((host, port)).await?;
-
-    let request = format!("GET {} HTTP/1.1\r\nHost: {}\r\n\r\n", path, host);
-    socket.write_all(request.as_bytes()).await?;
-    socket.shutdown(net::Shutdown::Write)?;
-
-    let mut response = String::new();
-    socket.read_to_string(&mut response).await?;
-
-    Ok(response)
-}
-
-async fn many_requests(requests: Vec<(String, u16, String)>) -> Vec<std::io::Result<String>> {
-    use async_std::task;
+pub async fn many_requests(urls: &[String]) -> Vec<Result<String, surf::Exception>> {
+    let client = surf::Client::new();
 
     let mut handles = vec![];
-    for (host, port, path) in requests {
-        handles.push(task::spawn_local(async move {
-            cheapo_request(&host, port, &path).await
-        }));
+    for url in urls {
+        let request = client.get(&url).recv_string();
+        handles.push(task::spawn(request));
     }
 
     let mut results = vec![];
@@ -33,10 +18,10 @@ async fn many_requests(requests: Vec<(String, u16, String)>) -> Vec<std::io::Res
 }
 
 fn main() {
-    let requests = vec![
-        ("example.com".to_string(), 80, "/".to_string()),
-        ("www.red-bean.com".to_string(), 80, "/".to_string()),
-        ("en.wikipedia.org".to_string(), 80, "/".to_string()),
+    let requests = &[
+        "http://example.com".to_owned(),
+        "https://www.red-bean.com".to_owned(),
+        "https://en.wikipedia.org/wiki/Main_Page".to_owned(),
     ];
 
     let results = async_std::task::block_on(many_requests(requests));
